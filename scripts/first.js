d69a70b75b3b248f4f1d30322ffd3a250a82d24a
Khang Dinh Hoang <khangdh95@gmail.com>
2019-07-16 22:09:16 +0700

    test get info

--- scripts/first.js
+++ scripts/first.js
@@ -1,1 +1,10 @@
-window.location = "./webar.html"
+var content = "";
+
+content += "<p> name: " + platform.name; // 'Safari'
+content += "</p><p> version: " + platform.version; // '5.1'
+content += "</p><p> product: " + platform.product; // 'iPad'
+content += "</p><p> manufacturer: " + platform.manufacturer; // 'Apple'
+content += "</p><p> layout: " + platform.layout; // 'WebKit'
+content += "</p><p> os family: " + platform.os.family; // 'iOS 5.0'
+
+document.write("</p> Chào mừng!" + content);
